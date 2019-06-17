a44fd21f16e9753e88b6009b461c7394d708bb4b
Dhillon Wilkinson <dhillonwilk@gmail.com>
2019-06-17 21:32:29 +0100

    Added editor.is_command_mode to allow for cleaner code

--- src/editor.rs
+++ src/editor.rs
@@ -1,9 +1,9 @@
 // red editor
 // Copyright (c) animojis 2019-forever
 
-use pancurses::{Window, Input};
+use pancurses::{Input, Window};
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Copy, Clone)]
 pub enum EditorMode {
     Command,
     Insert,
@@ -12,8 +12,8 @@ pub enum EditorMode {
 
 pub struct Editor {
     window: Window,
-    content_window: Window,  
-    curr_mode: EditorMode
+    content_window: Window,
+    curr_mode: EditorMode,
 }
 
 impl Editor {
@@ -25,30 +25,39 @@ impl Editor {
     /// - `content_window` - The content sub-window to use
     ///
     pub fn new(window: Window, content_window: Window) -> Editor {
-        Editor { window: window, content_window: content_window, curr_mode: EditorMode::Insert }
-    }
-
-    /// Checks if the current mode allows editing or not
-    /// 
-    /// Returns `true` if the current mode allows editing, `false`
-    /// if it doesn't allow editing.
-    pub fn can_edit(&self) -> bool {
-        match &self.curr_mode {
-            EditorMode::Command => true, // true, but not to text buffer
-            EditorMode::Insert => true,
-            EditorMode::Cursor => false
+        Editor {
+            window: window,
+            content_window: content_window,
+            curr_mode: EditorMode::Insert,
         }
     }
 
+    /// Checks if the current mode allows editing or not
+    ///
+    /// Returns `true` if the current mode allows editing, `false`
+    /// if it doesn't allow editing.
+    pub fn can_edit(&self) -> bool {
+        match self.curr_mode {
+            EditorMode::Command => true, // true, but not to text buffer
+            EditorMode::Insert => true,
+            EditorMode::Cursor => false,
+        }
+    }
+
+    /// Checks if the editor is in command mode or not.
+    pub fn is_command_mode(&self) -> bool {
+        self.get_curr_mode() == EditorMode::Command
+    }
+
     /// Gets the main window associated with the editor
-    /// 
+    ///
     /// This is not the content window, and cannot be scrolled.
     pub fn get_window(&self) -> &Window {
         &self.window
     }
 
     /// Gets the content window associated with the editor
-    /// 
+    ///
     /// Unlike the main window (which can be got using `get_window`),
     /// the content window supports scrolling when the content is larger
     /// than itself.
@@ -57,19 +66,19 @@ impl Editor {
     }
 
     /// Gets the current editor mode. Can be one of the following
-    /// 
+    ///
     /// - `EditorMode::Command`- Editor is in command mode, text is interpreted as commands
     /// - `EditorMode::Insert` - Editor is insert mode, text can be inserted
     /// - `EditorMode::Cursor` - Editor is in cursor mode, arrow-keys navigation
-    /// 
+    ///
     /// In command mode, although editing is allowed any text that is inputted is not sent to
-    /// the text buffer and is instead treated as a command and handled as such. 
-    pub fn get_curr_mode(&self) -> &EditorMode {
-        &self.curr_mode
+    /// the text buffer and is instead treated as a command and handled as such.
+    pub fn get_curr_mode(&self) -> EditorMode {
+        self.curr_mode
     }
 
     /// Sets the current editor mode. Can be one of the following
-    /// 
+    ///
     /// - `EditorMode::Command`- Editor is in command mode, text is interpreted as commands
     /// - `EditorMode::Insert` - Editor is insert mode, text can be inserted
     /// - `EditorMode::Cursor` - Editor is in cursor mode, arrow-keys navigation
--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,17 @@
 mod editor;
 
 use editor::{Editor, EditorMode};
-use pancurses::{Window, Input};
+use pancurses::{Input, Window};
 
 fn main() {
     let window = pancurses::initscr();
     let size = window.get_max_yx();
 
     // Create a subwindow to hold the contents
-    let content_window = window.subwin(size.0, size.1, 0, 0)
+    let content_window = window
+        .subwin(size.0, size.1, 0, 0)
         .expect("Error creating scroll_window");
-    
+
     content_window.keypad(true);
     content_window.scrollok(true);
     content_window.setscrreg(0, size.0);
@@ -30,25 +31,34 @@ fn main() {
             Some(Input::Character('\u{1b}')) => {
                 // Exit to command mode
                 match editor.get_curr_mode() {
+                    // Already in command mode, don't have to do anything
                     EditorMode::Command => (),
 
-                    // Any other mode should be set the command mode when we hit ESC
-                    _ => { editor.set_curr_mode(EditorMode::Command); }
+                    // Set the current mode to the command mode when we hit ESC
+                    _ => {
+                        editor.set_curr_mode(EditorMode::Command);
+                    }
                 };
-            },
+            }
             Some(Input::KeyBackspace) => {
                 // Move to previous char and delete it
                 content_win.mv(content_win.get_cur_y(), content_win.get_cur_x() - 1);
                 content_win.delch();
                 content_win.refresh();
-            },
+            }
             Some(Input::KeyUp) => {
                 // Scroll the content window up
                 content_win.mvwin(content_win.get_cur_y() - 1, content_win.get_cur_x());
-                content_win.mv(0,0);
+                content_win.mv(0, 0);
                 content_win.refresh();
             }
-            Some(Input::Character(c)) => { content_win.addch(c); }
+            Some(Input::Character(c)) => {
+
+                if (editor.is_command_mode()) {
+                    // We are now in command mode
+                }
+                content_win.addch(c);
+            }
             Some(_) => (),
             None => (),
         }
